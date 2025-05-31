71eecce77a318c939560e4cea5513a6f0976dd6d
shilpi bohra <shilpibohra@gmail.com>
2025-05-31 19:10:42 +0530

    “ME_MERN_XEXIT_MODULE_ONE”

--- backend/helpers/dbconnect.js
+++ backend/helpers/dbconnect.js
@@ -1,8 +1,17 @@
-import mongoose from "mongoose";
+/*import mongoose from "mongoose";
 
 // const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.zjels.mongodb.net/exitease`;
 const uri = `mongodb+srv://shilpibohra:crioproject@cluster0.juw7s7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 export default mongoose
   .connect(uri)
-  .then(() => console.log("DB connection established"));
+  .then(() => console.log("DB connection established"));*/
+  import mongoose from "mongoose";
+import "dotenv/config";
+
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.juw7s7h.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
+export default mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
